ea0d0a23498edf25e748338f954afa727bb1670f
spacesouls <spacesouls@163.com>
2023-07-27 15:20:11 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,30 @@
 import { resolve } from 'path'
+import fs from 'fs'
 import { createUnplugin } from 'unplugin'
 import type { ResolvedConfig } from 'vite'
 
+export interface Options {
+  stylesPath?: string     //相对于input目录
+}
 
-export default createUnplugin((): any => {
-  let inputPath: any
+export default createUnplugin((options?: Options): any => {
+  let mainPath: string
+  let stylesPath: string
+  let styles: string
   return {
     name: 'unplugin-import-global-css',
     enforce: "post",
     configResolved(config: ResolvedConfig) {
       const root = config.root
       const path = resolve(root, './src/main.ts')
-      inputPath = normalizePath(path)
-      console.log('----',inputPath)
+      mainPath = normalizePath(path)
+      stylesPath = resolve(root, './src/styles')
+      styles = getStylesStr(stylesPath)
     },
     transform(code, id) {
-      if (inputPath == id) {
-        console.log('id', id)
+      if (mainPath == id) {
+        console.log(styles)
+        return code + styles
       }
     }
     
@@ -26,4 +34,17 @@ export default createUnplugin((): any => {
 function normalizePath(path: string):string {
   path = path.replaceAll('\\','/')
   return path
+}
+
+function readStylesFile(path): string[] {
+  return fs.readdirSync(path)
+}
+
+function getStylesStr(path: string): string {
+  const stylesArr = readStylesFile(path)
+  let styles = ''
+  stylesArr.forEach((style) => {
+    styles += `import './styles/${style}';`
+  })
+  return styles
 }
